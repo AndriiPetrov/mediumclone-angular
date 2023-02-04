ed9effcea26750739d326155c85f96953cb1d9fb
Andrii_Petrov2 <andrii_petrov2@epam.com>
2023-02-04 19:11:37 +0200

    Add prefix for the generate component and make the app NgModul-less

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import {CommonModule} from '@angular/common'
+import {RouterModule} from '@angular/router'
 import {Component} from '@angular/core'
 
 @Component({
-  selector: 'app-root',
+  selector: 'medium-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
 })
 export class AppComponent {
   title = 'mediumclone-angular'
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
+// import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
 
-import {AppModule} from './app/app.module'
+import {bootstrapApplication} from '@angular/platform-browser'
+import {AppComponent} from './app/app.component'
+import {provideRouter, Routes} from '@angular/router'
+import {RegisterComponent} from './app/auth/components/register/register.component'
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err))
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch((err) => console.error(err))
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+]
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+}).catch((err) => console.error(err))
