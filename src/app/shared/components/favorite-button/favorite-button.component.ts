5fb45a4391e5dceb9486d8cdd39cea9247ac5127
Andrii_Petrov2 <andrii_petrov2@epam.com>
2023-03-01 18:39:07 +0200

    added routing to the main page if you not login when click favourite button

--- src/app/shared/components/favorite-button/favorite-button.component.ts
+++ src/app/shared/components/favorite-button/favorite-button.component.ts
@@ -1,7 +1,10 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { addToFavoritesAction } from './actions/add-to-favorites.actions';
+import { isLoggedInSelector } from 'src/app/auth/store/selectors';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'medium-favorite-button',
@@ -10,34 +13,49 @@ import { addToFavoritesAction } from './actions/add-to-favorites.actions';
   templateUrl: './favorite-button.component.html',
   styleUrls: ['./favorite-button.component.scss'],
 })
-export class FavoriteButtonComponent implements OnInit {
+export class FavoriteButtonComponent implements OnInit, OnDestroy {
   @Input() articleSlug: string;
   @Input() isFavorited: boolean;
   @Input() favoritesCount: number;
 
   count: number;
   isFavor: boolean;
+  isLoggedIn: boolean;
+
+  isLoggedInSubscription: Subscription;
 
   store = inject(Store);
+  router = inject(Router);
 
   ngOnInit(): void {
     this.count = this.favoritesCount;
     this.isFavor = this.isFavorited;
+    this.isLoggedInSubscription = this.store
+      .select(isLoggedInSelector)
+      .subscribe((isLoggedIn) => (this.isLoggedIn = isLoggedIn));
+  }
+
+  ngOnDestroy(): void {
+    this.isLoggedInSubscription.unsubscribe();
   }
 
   handleLike(): void {
-    this.store.dispatch(
-      addToFavoritesAction({
-        isFavorited: this.isFavor,
-        slug: this.articleSlug,
-      })
-    );
-    if (this.isFavor) {
-      this.count--;
-    } else {
-      this.count++;
-    }
+    if (this.isLoggedIn) {
+      this.store.dispatch(
+        addToFavoritesAction({
+          isFavorited: this.isFavor,
+          slug: this.articleSlug,
+        })
+      );
+      if (this.isFavor) {
+        this.count--;
+      } else {
+        this.count++;
+      }
 
-    this.isFavor = !this.isFavor;
+      this.isFavor = !this.isFavor;
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
 }
